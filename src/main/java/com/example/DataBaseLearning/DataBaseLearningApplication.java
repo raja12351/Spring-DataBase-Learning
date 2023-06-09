e8510cd025243470ac0b8272b046e82e74a135b8
raja12351 <rajatsingh12351@gmail.com>
2023-06-10 00:41:33 +0530

    Practice

---
+++ src/main/java/com/example/DataBaseLearning/DataBaseLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataBaseLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseLearningApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DataBaseLearning/User.java
@@ -0,0 +1,51 @@
+package com.example.DataBaseLearning;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private String hobby;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
